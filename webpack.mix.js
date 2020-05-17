b8a644b125a64ff83bdae0ae1fd735d2c642f649
Christoph Schweppe <info@cschweppe.de>
2020-05-17 15:12:28 +0200

    Update css to latest changes
    And remove Tailwind

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,7 @@
 const mix = require('laravel-mix');
-const tailwindcss = require('tailwindcss');
 
  mix.sass('resources/src/styles.scss', 'resources/dist')
 .options({
     processCssUrls: false,
-    postCss: [ tailwindcss('./vendor/tcg/voyager/tailwind.config.js') ],
+    postCss: [],
 });
